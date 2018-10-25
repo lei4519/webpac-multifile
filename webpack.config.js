47b89fadfaf46f301d237507a6fe62ebd9ffbcd9
lei4519 <939690017@qq.com>
2018-10-25 14:52:11 +0800

    start

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: {
+		index: './src/js/index.js',
+		cart: './src/js/cart.js'
+	},
+	output: {
+		filename: 'js/[name].js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: ''
+	},
+	module: {
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html',
+			chunks: ['index']
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'cart.html',
+			template: './src/cart.html',
+			chunks: ['cart']
+		})
+	]
+	// devtool: '#source-map'
+}
